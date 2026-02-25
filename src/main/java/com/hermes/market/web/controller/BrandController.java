327e409a8102ddcc15f89183ef377f8cc616f3e0
VictorN7 <victorhnogueira@hotmail.com>
2026-02-25 18:33:06 -0300

    feat: Create a brandController class for the Get endpoints of the brand, communicating with BrandService and tested with H2.

--- src/main/java/com/hermes/market/web/controller/BrandController.java
+++ src/main/java/com/hermes/market/web/controller/BrandController.java
@@ -1,14 +1,32 @@
 package com.hermes.market.web.controller;
 
+import com.hermes.market.application.service.BrandService;
 import com.hermes.market.domain.product.Brand;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/brands")
 public class BrandController {
 
+    private final BrandService brandService;
 
+    public BrandController(BrandService brandService){
+        this.brandService =  brandService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Brand>> findAll(){
+     return ResponseEntity.ok().body(brandService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Brand> findById(@PathVariable Long id){
+        return ResponseEntity.ok().body(brandService.findById(id));
+    }
 }
