041c88c6433171394ac424cdfc80813b519819c0
VictorN7 <victorhnogueira@hotmail.com>
2026-03-08 01:52:41 -0300

    feat: add promotion status enum

---
+++ src/main/java/com/hermes/market/domain/product/PromotionStatus.java
@@ -0,0 +1,27 @@
+package com.hermes.market.domain.product;
+
+public enum PromotionStatus {
+
+    ACTIVE(1),
+    INACTIVE(2);
+
+    private final int code;
+
+    PromotionStatus(int code){
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static PromotionStatus fromCode(int code){
+
+        for (PromotionStatus status : PromotionStatus.values()){
+            if (status.getCode() == code){
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Invalid PromotionStatus code"+code);
+    }
+}
