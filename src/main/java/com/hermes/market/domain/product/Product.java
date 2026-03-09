ae5f6c4affd83fe98ea4aa3b06bf9df468c8613e
VictorN7 <victorhnogueira@hotmail.com>
2026-03-09 00:23:25 -0300

    feat: add ManyToMany relationship between Product and Promotion

--- src/main/java/com/hermes/market/domain/product/Product.java
+++ src/main/java/com/hermes/market/domain/product/Product.java
@@ -3,16 +3,10 @@ package com.hermes.market.domain.product;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.Instant;
+import java.util.List;
 import java.util.Objects;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_products")
@@ -51,6 +45,8 @@ public class Product {
 	@JoinColumn(name = "brand_id", nullable = false)
 	private Brand brand;
 
+	@ManyToMany(mappedBy = "products")
+	private List<Promotion> promotions;
 
 	public Product() {
 	}
