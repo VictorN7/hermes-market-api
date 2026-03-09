419aca3fbe12c0d3f2222c391c0d228f9a8d320c
VictorN7 <victorhnogueira@hotmail.com>
2026-03-09 02:42:24 -0300

    feat: add DTO PromotionResponse

---
+++ src/main/java/com/hermes/market/application/dto/response/PromotionResponse.java
@@ -0,0 +1,87 @@
+package com.hermes.market.application.dto.response;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public class PromotionResponse {
+
+    private Long id;
+    private String name;
+    private Instant startDate;
+    private Instant endDate;
+    private String status;
+    private String type;
+    private BigDecimal promotionalPrice;
+    private Integer minQuantity;
+    private BigDecimal discountedUnitPrice;
+
+    public PromotionResponse(Long id, String name, Instant startDate, Instant endDate,
+                             String status, String type, BigDecimal promotionalPrice,
+                             Integer minQuantity, BigDecimal discountedUnitPrice) {
+        this.id = id;
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
+        this.type = type;
+        this.promotionalPrice = promotionalPrice;
+        this.minQuantity = minQuantity;
+        this.discountedUnitPrice = discountedUnitPrice;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Instant getStartDate() {
+        return startDate;
+    }
+
+    public Instant getEndDate() {
+        return endDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public BigDecimal getPromotionalPrice() {
+        return promotionalPrice;
+    }
+
+    public Integer getMinQuantity() {
+        return minQuantity;
+    }
+
+    public BigDecimal getDiscountedUnitPrice() {
+        return discountedUnitPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "PromotionResponse{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", status='" + status + '\'' +
+                ", type='" + type + '\'' +
+                ", promotionalPrice=" + promotionalPrice +
+                ", minQuantity=" + minQuantity +
+                ", discountedUnitPrice=" + discountedUnitPrice +
+                '}';
+    }
+}
