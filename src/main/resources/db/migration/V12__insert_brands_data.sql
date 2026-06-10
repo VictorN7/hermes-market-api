02bb9c5ec64153e5761818fcda8113d4fd5d7347
VictorN7 <victorhnogueira@hotmail.com>
2026-06-09 23:54:38 -0300

    feat: add initial brand seed data

---
+++ src/main/resources/db/migration/V12__insert_brands_data.sql
@@ -0,0 +1,41 @@
+INSERT INTO tb_brands (name, status, created_at)
+VALUES ('Ypê', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_brands (name, status, created_at)
+VALUES ('Fazendinha YAYA', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_brands (name, status, created_at)
+VALUES ('Colheita Feliz', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_brands (name, status, created_at)
+VALUES ('Coca-Cola', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_brands (name, status, created_at)
+VALUES ('Nestlé', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_brands (name, status, created_at)
+VALUES ('Quaker', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_brands (name, status, created_at)
+VALUES ('Sadia', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_brands (name, status, created_at)
+VALUES ('Seara', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_brands (name, status, created_at)
+VALUES ('Perdigão', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_brands (name, status, created_at)
+VALUES ('Vigor', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_brands (name, status, created_at)
+VALUES ('Itambé', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_brands (name, status, created_at)
+VALUES ('Pullman', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_brands (name, status, created_at)
+VALUES ('Gillette', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_brands (name, status, created_at)
+VALUES ('Xereta', 1, CURRENT_TIMESTAMP);
