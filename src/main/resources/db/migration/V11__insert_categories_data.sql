b06793f79e7ad9314371524cfdaa13b8df028a04
VictorN7 <victorhnogueira@hotmail.com>
2026-06-09 23:52:05 -0300

    feat: add initial category seed data

---
+++ src/main/resources/db/migration/V11__insert_categories_data.sql
@@ -0,0 +1,29 @@
+INSERT INTO tb_categories (name, status, created_at)
+VALUES ('Hortifruti', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_categories (name, status, created_at)
+VALUES ('Bebidas', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_categories (name, status, created_at)
+VALUES ('Limpeza', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_categories (name, status, created_at)
+VALUES ('Grãos e Cereais', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_categories (name, status, created_at)
+VALUES ('Laticínios', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_categories (name, status, created_at)
+VALUES ('Carnes e Aves', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_categories (name, status, created_at)
+VALUES ('Padaria e Confeitaria', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_categories (name, status, created_at)
+VALUES ('Higiene Pessoal', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_categories (name, status, created_at)
+VALUES ('Frios e Embutidos', 1, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_categories (name, status, created_at)
+VALUES ('Congelados', 1, CURRENT_TIMESTAMP);
