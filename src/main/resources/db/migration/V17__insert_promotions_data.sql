86c7402272b1ea656d5c1e1a18ea5130c1d7f9d7
VictorN7 <victorhnogueira@hotmail.com>
2026-06-20 23:45:03 -0300

    fix: flyway migration duplicate insert error

--- src/main/resources/db/migration/V17__insert_promotions_data.sql
+++ src/main/resources/db/migration/V17__insert_promotions_data.sql
@@ -1,6 +1,6 @@
 INSERT INTO tb_promotions
 (name, start_date, end_date, status, type, discount_percentage, min_quantity)
 VALUES
-    ('Oferta de Carnes', '2026-06-20T00:00:00Z','2026-06-28T00:00:00Z', 1, 2, 15, 3),
+    ('Semana Nestlé', '2026-06-20T00:00:00Z','2026-06-28T00:00:00Z', 1, 2, 15, 3),
     ('Promoção Bebidas da Semana', '2026-06-21T00:00:00Z','2026-06-25T00:00:00Z', 1, 1, 5, null),
     ('Oferta de Carnes', '2026-06-23T00:00:00Z','2026-06-25T00:00:00Z', 1, 1, 10, null);
