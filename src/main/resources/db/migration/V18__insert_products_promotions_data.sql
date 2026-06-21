9e8c80cbc90709f3cafbacc7964e9904e1395f2b
VictorN7 <victorhnogueira@hotmail.com>
2026-06-20 23:22:47 -0300

    feat: associate products with their respective promotions

---
+++ src/main/resources/db/migration/V18__insert_products_promotions_data.sql
@@ -0,0 +1,15 @@
+
+-- Promotion 1 - Semana Nestlé
+
+INSERT INTO tb_products_promotions (product_id, promotion_id)
+VALUES (16,1),(17,1),(19,1);
+
+-- Promotion 2 - Promoção Bebidas da Semana
+
+INSERT INTO tb_products_promotions (product_id, promotion_id)
+VALUES (11,2),(15,2),(20,2);
+
+-- Promotion 3 - Oferta de Carnes
+
+INSERT INTO tb_products_promotions (product_id, promotion_id)
+VALUES (51,3),(53,3);
