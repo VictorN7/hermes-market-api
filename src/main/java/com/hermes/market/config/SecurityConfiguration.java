68ad3605a95d3e8557b7769008e48e9f70ad4ec6
VictorN7 <victorhnogueira@hotmail.com>
2026-06-21 23:27:44 -0300

    feat: Basic Spring Security configuration implemented

--- src/main/java/com/hermes/market/config/SecurityConfiguration.java
+++ src/main/java/com/hermes/market/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.hermes.market.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,8 +17,18 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        //Habilita formulario de login e Http basic para usar no postman e qualquer requisição é autorizada
+
+        // Desabilitado o CSRF
+        // Formulario de login habilitado
+        // httpbasic para requisições no Postman
+        // Qualquer requisição precisa de autorização
+
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
                 .build();
     }
 
