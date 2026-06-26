16ccbefe78ba4acd5cd636ef3e479bc96e171341
VictorN7 <victorhnogueira@hotmail.com>
2026-06-26 01:12:41 -0300

    refactor: Refactored the project to be a REST API only, without the MVC pattern involving HTML and CSS

--- src/main/java/com/hermes/market/config/SecurityConfiguration.java
+++ src/main/java/com/hermes/market/config/SecurityConfiguration.java
@@ -12,24 +12,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    //Bean do tipo SecurityFilterChain
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        //Habilita formulario de login e Http basic para usar no postman e qualquer requisição é autorizada
-
-        // Desabilitado o CSRF
-        // Formulario de login habilitado
-        // httpbasic para requisições no Postman
-        // Qualquer requisição precisa de autorização
-
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(Customizer.withDefaults())
-         //       .httpBasic(Customizer.withDefaults())
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated()
+                ).httpBasic(Customizer.withDefaults())
                 .build();
     }
-
 }
